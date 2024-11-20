cd21d4b6ef3ed162333732d082510a28015e7554
MOHAMMADALSIFAN <alsifan01@gmail.com>
2024-11-20 10:49:49 +0800

    first

---
+++ src/main/java/sg/edu/nus/iss/vtpp5a_ssf_day_13/Vtpp5aSsfDay13Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vtpp5a_ssf_day_13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vtpp5aSsfDay13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vtpp5aSsfDay13Application.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/vtpp5a_ssf_day_13/Vtpp5aSsfDay13ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.vtpp5a_ssf_day_13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vtpp5aSsfDay13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
